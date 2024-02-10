b46f8ca0819752caeb4ca62fe4e5e3930633a08c
KobbyGod <159494748+KobbyGod@users.noreply.github.com>
2024-02-09 18:38:42 -0600

    Add files via upload

---
+++ Eva1_2_IDENTIFICADORES/src/eva2_2_identificadores/Eva2_2_IDENTIFICADORES.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_2_identificadores;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva2_2_IDENTIFICADORES extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        int edad;
+        String Nombre;
+        double salario;
+        //ASIGNACION DE VALORES;
+        //IDENTIFICADOR = VALOR;
+        //se coloca el valor 19 dentro de la variable edad;
+        edad = 19;
+        Nombre = "KobbyGod";
+        salario = 123.3;
+        //COMO LOS USAMOS
+            System.out.println(Nombre);
+            System.out.println(edad);
+            System.out.println(salario);
+    }
+    
+}
